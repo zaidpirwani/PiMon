0511909b972d5391dca1cd79d63192ef7eb2ee50
Zaid Pirwani <email@zaidpirwani.com>
2016-06-06 21:15:28 +0500

    Threading class works, FIX display bugs

--- piMon.py
+++ piMon.py
@@ -47,40 +47,58 @@ for btn in btns:
 	GPIO.setup(btn, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 	GPIO.add_event_detect(btn,GPIO.FALLING,callback=btnPressed,bouncetime=250)
 
+import threading
+
 import Adafruit_DHT
 sensor = Adafruit_DHT.DHT11
 pin = 17
-def dht11(delay):
-	while True:
-		time.sleep(delay)
-		humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+class dht11ThreadClass(threading.Thread):
+	def __init__(self, delay):
+		threading.Thread.__init__(self)
+		self.delay = delay
+	def run(self):
+		while True:
+			time.sleep(self.delay)
+			global humidity, temperature
+			humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+dht11Thread = dht11ThreadClass(15)
+dht11Thread.start()
 
-def dsb1820(delay):
-	while True:
-		time.sleep(delay)
-		tfile = open("/sys/bus/w1/devices/28-031574561eff/w1_slave")
-		text = tfile.read()
-		tfile.close()
-		temperature2_data = text.split()[-1]
-		temperature2 = float(temperature2_data[2:])
-		temperature2 = temperature2 / 1000
-
-#http://www.tutorialspoint.com/python/python_multithreading.htm
-import thread
-
-try:
-	thread.start_new_thread( dht11,   (15) )
-	thread.start_new_thread( dsb1820, (15) )
-except:
-	print "Error: unable to start thread"
+class ds18b20ThreadClass(threading.Thread):
+	def __init__(self, delay):
+		threading.Thread.__init__(self)
+		self.delay = delay
+	def run(self):
+		while True:
+			time.sleep(self.delay)
+			tfile = open("/sys/bus/w1/devices/28-031574561eff/w1_slave")
+			text = tfile.read()
+			tfile.close()
+			temperature2_data = text.split()[-1]
+			global temperature2
+			temperature2 = float(temperature2_data[2:])
+			temperature2 = temperature2 / 1000
+ds18b20Thread = ds18b20ThreadClass(15)
+ds18b20Thread.start()
 
 import lcd
-x=0
-y=8
-lcd.Init()
-lcd.Print(x,y*0, ssid)
-lcd.Print(x,y*1, IP)
-lcd.Print(x,y*2, dt.datetime.now().strftime('%d/%m')+' - '+dt.datetime.now().strftime('%H:%M'))
+class lcdThreadClass(threading.Thread):
+	def __init__(self, delay):
+		threading.Thread.__init__(self)
+		self.delay = delay
+	def run(self):
+		while True:
+			time.sleep(self.delay)
+			x=0
+			y=8
+			lcd.Init()
+			lcd.Print(x,y*0, ssid)
+			lcd.Print(x,y*1, IP)
+			lcd.Print(x,y*2, dt.datetime.now().strftime('%d/%m')+' - '+dt.datetime.now().strftime('%H:%M'))
+			lcd.Print(x,y*3, 'T1=' + "{:.1f}".format(temperature) + ' - T2=' + "{:.1f}".format(temperature2))
+			lcd.Print(x,y*4, 'H1=' + "{:.1f}".format(humidity))
+lcdThread = lcdThreadClass(15)
+lcdThread.start()
 
 while True:
 	time.sleep(15)
